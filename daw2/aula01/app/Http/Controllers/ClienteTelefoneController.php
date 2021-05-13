fa1126c6da58330a4267c830be3a7d7a0eac4730
OtavioHypolito <otaviohypolito@hotmail.com>
2021-05-13 18:43:11 -0300

    Atividade2

---
+++ daw2/aula01/app/Http/Controllers/ClienteTelefoneController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ClienteTelefone;
+
+class ClienteTelefoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+		
+        $cliente_telefone = new ClienteTelefone();
+		$cliente_telefone->cliente = $request->get("cliente");
+		
+		$cliente_telefones = ClienteTelefone::Where("cliente", $request->get("cliente"))->orderByDesc("preferencial") ->get();
+		
+		return view("cliente_telefone.index", [
+			"cliente_telefone" => $cliente_telefone,
+			"cliente_telefones" => $cliente_telefones
+		]);
+		
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if ($request->get("id") != "") {
+			$cliente_telefone = ClienteTelefone::Find($request->get("id"));
+		} else {
+			$cliente_telefone = new ClienteTelefone();
+		}
+		
+		$cliente_telefone->cliente = $request->get("cliente");
+
+        $telefone = $request->get("telefone"); 
+        $telefone = str_replace("(", "", $telefone);
+        $telefone = str_replace(")", "", $telefone);
+        $telefone = str_replace("-", "", $telefone);
+        $telefone = str_replace(" ", "", $telefone);
+        
+        
+		$cliente_telefone->telefone = $telefone;
+
+        if($request->get("preferencial") == 1){
+            $cliente_telefone->preferencial = 1;
+        } else {
+            $cliente_telefone->preferenCial = 0;
+        }
+		
+		$cliente_telefone->save();
+		
+		$request->session()->flash("status", "salvo");
+		
+		return redirect()->action(
+			[ClienteTelefoneController::class, "index"], [ "cliente" => $request->get("cliente") ]
+		);
+		
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $id)
+    {
+        $cliente_telefone = ClienteTelefone::Find($id);
+		$cliente = $cliente_telefone->cliente;
+		
+		$cliente_telefone->delete();
+		$request->session()->flash("status", "excluido");
+		
+		return redirect()->action(
+			[ClienteTelefoneController::class, "index"], [ "cliente" => $cliente ]
+		);
+		
+    }
+}
---
+++ daw2/aula01/resources/views/cliente_telefone/index.blade.php
@@ -0,0 +1,92 @@
+@extends("template.popup")
+
+@section("cadastro")
+	
+	<form action="/cliente_telefone" method="POST" class="row">
+		
+		@csrf
+		<div class="form-group col-6">
+			<label for="telefone">Telefone: </label>
+			<input type="text" id="telefone" name="telefone" value="{{ $cliente_telefone->telefone }}" class="form-control" />
+		</div>
+		
+		<div class="form-group col-6">
+			
+			<label for="preferencial">
+				<input type="checkbox" name="preferencial" value="1"/>
+				Telefone Preferencial 
+			</label><br/>
+				
+			<a class="btn btn-primary" href="/cliente_telefone?cliente={{ $cliente_telefone->cliente }}">Novo</a>
+			<input type="hidden" id="id" name="id" value="{{ $cliente_telefone->id }}" />
+			<input type="hidden" id="cliente" name="cliente" value="{{ $cliente_telefone->cliente }}" />
+			<button class="btn btn-success" type="submit">Salvar</button>
+		</div>
+	</form>
+	
+	<script>
+		$(document).ready(function() {
+			var options =  {
+				onKeyPress: function(telefone, e, field, options) {
+					var masks = [ '(00) 00000-0000', '(00) 0000-00009' ];
+					var mask = (telefone.replace(/\D/g, '').length == 11) ? masks[0] : masks[1];
+					$('#telefone').mask(mask, options);
+				}
+			};
+			$('#telefone').mask('(00) 0000-00009', options);
+			
+			$("span.telefone").each(function() {
+				var len = $(this).html().trim().length;
+				if (len == 1) {
+					$(this).mask('(00) 00000-0000', options);
+				} else {
+					$(this).mask('(00) 0000-0000', options);
+				}
+			});
+			
+			
+		});
+	</script>
+	
+@endsection
+
+@section("listagem")
+	
+	<table class="table table-striped">
+		
+		<thead>
+			<tr>
+				<th>Telefone</th>
+				<th>Del</th>
+			</tr>
+		</thead>
+		
+		<tbody>
+			
+		@foreach ($cliente_telefones as $cliente_telefone)
+				
+				<tr>
+					<td>
+						@if ($cliente_telefone->preferencial == 1)
+							(*)
+						@endif
+						<span class="telefone">
+							{{ $cliente_telefone->telefone }} 
+						</span>
+					</td>
+					<td>
+						<form action="/cliente_telefone/{{ $cliente_telefone->id }}" method="POST">
+							@csrf
+							<input type="hidden" name="_method" value="DELETE" />
+							<button  class="btn btn-danger" type="submit" onclick="return confirm('Tem Certeza?');">Del</button>
+						</form>
+					</td>
+				</tr>
+				
+			@endforeach
+			
+		</tbody>
+		
+	</table>
+	
+@endsection
